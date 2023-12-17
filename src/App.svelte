a06bb79cc26efaecf281a7c12c3f380535cba6f9
HVCsano <55344602+HVCsano@users.noreply.github.com>
2023-12-17 10:54:56 +0100

    watermark fix

--- src/App.svelte
+++ src/App.svelte
@@ -82,8 +82,40 @@
       <button type="submit" class="bg-blue-500 duration-200 mt-2 hover:bg-blue-700 rounded-lg text-lg w-28 p-1">Számolás</button>
     </form>
   </div>
+  <div class="bg-blue-600 md:mx-56 xl:mx-96 mx-2 p-5 my-5 rounded-3xl">
+    <h2 class="text-2xl mb-2">Születési számoló</h2>
+    <h3>{#if szülszal.back !==undefined}{szülszal.back}{/if}% annak az esélye, hogy ketten egy napon születtek</h3>
+    <form on:submit|preventDefault={calcszülszal}>
+      <input type="number" required placeholder="emberek" class="text-white duration-200 focus:bg-blue-700 text-lg placeholder:text-white text-center w-28 rounded-lg p-1 bg-blue-500" bind:value={szülszal.emberek}>
+      <button type="submit" class="bg-blue-500 duration-200 mt-2 hover:bg-blue-700 rounded-lg text-lg w-28 p-1">Számolás</button>
+    </form>
+  </div>
+  <div class="bg-blue-600 md:mx-56 xl:mx-96 mx-2 p-5 my-5 rounded-3xl">
+    <h2 class="text-2xl mb-2">Születési számoló</h2>
+    <h3>{#if szülszal.back !==undefined}{szülszal.back}{/if}% annak az esélye, hogy ketten egy napon születtek</h3>
+    <form on:submit|preventDefault={calcszülszal}>
+      <input type="number" required placeholder="emberek" class="text-white duration-200 focus:bg-blue-700 text-lg placeholder:text-white text-center w-28 rounded-lg p-1 bg-blue-500" bind:value={szülszal.emberek}>
+      <button type="submit" class="bg-blue-500 duration-200 mt-2 hover:bg-blue-700 rounded-lg text-lg w-28 p-1">Számolás</button>
+    </form>
+  </div>
+  <div class="bg-blue-600 md:mx-56 xl:mx-96 mx-2 p-5 my-5 rounded-3xl">
+    <h2 class="text-2xl mb-2">Születési számoló</h2>
+    <h3>{#if szülszal.back !==undefined}{szülszal.back}{/if}% annak az esélye, hogy ketten egy napon születtek</h3>
+    <form on:submit|preventDefault={calcszülszal}>
+      <input type="number" required placeholder="emberek" class="text-white duration-200 focus:bg-blue-700 text-lg placeholder:text-white text-center w-28 rounded-lg p-1 bg-blue-500" bind:value={szülszal.emberek}>
+      <button type="submit" class="bg-blue-500 duration-200 mt-2 hover:bg-blue-700 rounded-lg text-lg w-28 p-1">Számolás</button>
+    </form>
+  </div>
+  <div class="bg-blue-600 md:mx-56 xl:mx-96 mx-2 p-5 my-5 rounded-3xl">
+    <h2 class="text-2xl mb-2">Születési számoló</h2>
+    <h3>{#if szülszal.back !==undefined}{szülszal.back}{/if}% annak az esélye, hogy ketten egy napon születtek</h3>
+    <form on:submit|preventDefault={calcszülszal}>
+      <input type="number" required placeholder="emberek" class="text-white duration-200 focus:bg-blue-700 text-lg placeholder:text-white text-center w-28 rounded-lg p-1 bg-blue-500" bind:value={szülszal.emberek}>
+      <button type="submit" class="bg-blue-500 duration-200 mt-2 hover:bg-blue-700 rounded-lg text-lg w-28 p-1">Számolás</button>
+    </form>
+  </div>
 </main>
 
-<div class="text-white absolute right-5 bottom-3">
+<div class="text-white fixed bottom-2 right-4">
   <h1 class="text-xl sticky">Ampix 2021-2023</h1>
 </div>
